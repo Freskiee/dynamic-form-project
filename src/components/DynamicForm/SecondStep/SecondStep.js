058ed98f1327dbfcffb7dc387019eabec68db0d6
Ariel Sanchez <freska_96@hotmail.com>
2024-07-08 10:52:51 -0600

    feat: add step two and three

--- src/components/DynamicForm/SecondStep/SecondStep.js
+++ src/components/DynamicForm/SecondStep/SecondStep.js
@@ -1,10 +1,51 @@
+import { useState } from "react";
+import { Col, Container, Form, Row } from "react-bootstrap";
+import Select from 'react-select';
 
-import './SecondStep.css';
+const options = [
+  { id: 1, value: "football", label: "Football ⚽" },
+  { id: 2, value: "computer", label: "Computer 💻" },
+  { id: 3, value: "play", label: "Play 🎮" },
+  { id: 4, value: "music", label: "Play Music 🎼" },
+  { id: 5, value: "photo", label: "Photography 📷" },
+  { id: 6, value: "swim", label: "Swim 🥽" },
+  { id: 7, value: "run", label: "Run 👟" },
+];
 
 export default function SecondStep() {
+
+  const [selectOption, setSelectOption] = useState(null);
+
+  const hobbiesSer = JSON.stringify(selectOption);
+  localStorage.setItem('hobbies', hobbiesSer);
+
+  const onChangeAdittionalHobbie = (e) => {
+    const sport = e.target.value;
+    localStorage.setItem('adittionalHobbie', sport);
+  };
+
   return (
     <div>
-      SecondStep
+      <h2>Selected your favorite sport</h2>
+      <h4>Tell me about your hobbies</h4>
+      <Container className="p-5 textt-center">
+        <Row className="justify-content-md-center">
+          <Col lg='7'>
+            <h5>Select your hobbies</h5>
+            <Select
+              defaultValue={selectOption}
+              onChange={setSelectOption}
+              options={options}
+              isSearchable
+              isMulti
+            />
+          </Col>
+          <Col lg='7' className="mt-4">
+            <h5>Do you want to add more?</h5>
+            <Form.Control type="text" placeholder="Karate" onChange={(e) => onChangeAdittionalHobbie(e)} />
+          </Col>
+        </Row>
+      </Container>
     </div>
-  )
-}
+  );
+};
